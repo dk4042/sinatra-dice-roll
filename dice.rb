286615b21cc55f196c8ef733f8d065c3820a1a40
dk4042 <dkhamaza@uchicago.edu>
2024-10-18 03:50:47 +0000

    committing

--- dice.rb
+++ dice.rb
@@ -1,21 +1,7 @@
 require "sinatra"
-get("/") do
-  "Hello World"
-end
-
-get("/home") do
-  "<h1>Dice Roll</h1>
-    
-  <ul>
-    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-  </ul>
-  "
-end
-
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
 
 
 get("/dice/2/6") do
@@ -23,10 +9,8 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	erb(:two_six, { :layout => :wrapper })
 end
 
 get("/dice/2/10") do 
@@ -58,4 +42,13 @@ get("/dice/5/4") do
   outcome = "You rolled a #{first_die}, a #{second_die}, a #{th_die}, a #{fo_die}, and a #{fi_die} for a total of #{sum}."
   "<h1>5d4</h1>
    <p>#{outcome}</p>"
+end
+
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+
+get("/") do
+  erb(:elephant)
 end
